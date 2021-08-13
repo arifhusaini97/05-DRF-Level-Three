98d0e9d3d27bcf8869f55a2ff3eeb421599654da
NUR ARIF HUSAINI BIN NORWAZA <chemist.arif.husaini@gmail.com>
2021-08-13 19:25:11 +0800

    Viewsets and Routers

--- api/profiles/api/urls.py
+++ api/profiles/api/urls.py
@@ -1,6 +1,34 @@
-from django.urls import path
-from profiles.api.views import ProfileList
+from django.urls import path, include
+# level 1
+# from profiles.api.views import ProfileList
+# level 2
+from profiles.api.views import (
+    ProfileViewSet,
+    ProfileStatusViewSet,
+    AvatarUpdateView,
+)
+# level 3
+from rest_framework.routers import DefaultRouter
+
+
+# level 2
+# profile_list = ProfileViewSet.as_view({"get": "list"})
+# profile_detail = ProfileViewSet.as_view({"get": "retrieve"})
+
+# level 3
+router = DefaultRouter()
+router.register(r"profiles", ProfileViewSet)
+router.register(r"status", ProfileStatusViewSet)
+
 
 urlpatterns = [
-    path("profiles/", ProfileList.as_view(), name="profile-list")
+    # level 1
+    # path("profiles/", ProfileList.as_view(), name="profile-list")
+    # level 2
+    # path("profiles/", profile_list, name="profile-list"),
+    # path("profiles/<int:pk>/", profile_detail, name="profile-detail")
+    # level 3
+    path("", include(router.urls)),
+    path("avatar/", AvatarUpdateView.as_view(), name="avatar-update")
+
 ]
